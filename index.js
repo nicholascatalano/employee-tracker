b0783db60fefd291294e9d08d636492511ce1c86
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-04 17:41:52 -0500

    adds initial inquirer prompt for user

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const inquirer = require("inquirer");
 const db = require("./db/connection");
 
 // Starts server after connecting to database
-db.connect(err => {
-    if (err) throw err;
-    console.log("Connected to database!");
-    employeeTracker();
-})
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to database!");
+  employeeTracker();
+});
 
 let employeeTracker = function () {
-    inquirer.prompt([{
-        
-    }])
-}
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "prompt",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Updated Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Quit",
+        ],
+      },
+    ])
+    .then((answers) => {});
+};
