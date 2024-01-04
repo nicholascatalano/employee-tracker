0a440a7371ffe6da37ec7fc7a8f0c526ee88d2d3
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-04 17:11:36 -0500

    adds seeding for role info to be seeded into role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,11 @@ VALUES
     ('Sales'),
     ('Finance'),
     ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Mechanical Engineer', 90000, 1),
+    ('Sales Leader', 110000, 2),
+    ('Financial Analyst', 80000, 3),
+    ('Lawyer', 125000, 4);
+
