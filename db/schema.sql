45d56bd903e2bc0149cff4e7c03d984f195628b3
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-04 16:55:28 -0500

    adds schema to create employee table

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INTEGER
 );
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
