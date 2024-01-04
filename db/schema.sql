82088156b1fbb5368a3acd5894cede9707878ea2
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-04 16:54:55 -0500

    adds schema to create role table

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,11 @@
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
 );
